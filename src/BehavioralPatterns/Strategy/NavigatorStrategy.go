427b422e90e59bb8e3045c1141aa13c63ac76a22
王孟宇 <wangmengyu04@maoyan.com>
2020-10-15 17:43:30 +0800

    update  Strategy

---
+++ src/BehavioralPatterns/Strategy/NavigatorStrategy.go
@@ -0,0 +1,80 @@
+package main
+
+import "fmt"
+
+/*
+更简单的策略模式的实现
+ */
+
+// 根据客户端选择不同的导航策略
+
+
+// 路径策略接口
+type RouteStrategy interface {
+
+	buildRoute()
+}
+
+type CarsStrategy struct {
+
+}
+
+func(s *CarsStrategy)buildRoute(){
+	fmt.Println("this a car navigator strategy")
+}
+
+
+type PublicTransportStrategy struct {
+
+}
+
+func(s *PublicTransportStrategy)buildRoute(){
+	fmt.Println("this a PublicTransport strategy")
+}
+
+
+type WalkingStrategy struct {
+
+}
+
+func(s *WalkingStrategy)buildRoute(){
+	fmt.Println("this a WalkingStrategy strategy")
+}
+
+
+type Navigator struct {
+
+	RouteStrategy RouteStrategy  // 将接口作为成员变量
+}
+
+
+func(n *Navigator)setRouteStrategy(r RouteStrategy){
+	n.RouteStrategy = r  // 由客户端调用此方法将导航策略设置到 Navigator 中
+}
+
+
+func routestrategyExecutor(evict string){
+	var n Navigator
+	switch evict {
+	case "car":
+		n.setRouteStrategy(&CarsStrategy{})
+	case "public":
+		n.setRouteStrategy(&PublicTransportStrategy{})
+	case "walking":
+		n.setRouteStrategy(&WalkingStrategy{})
+	}
+
+	n.RouteStrategy.buildRoute()
+
+}
+
+func main() {
+	//routestrategyExecutor("car")
+	//routestrategyExecutor("walking")
+	//routestrategyExecutor("public")
+	
+	var nav Navigator
+
+	nav.setRouteStrategy(&CarsStrategy{})
+	nav.RouteStrategy.buildRoute()
+}
---
+++ src/BehavioralPatterns/Strategy/Strategy.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 缓存策略接口
+type evictionAlgo interface {
+	evict(c *cache)
+}
+
+
+// 缓存策略的实现：fifo
+type fifo struct {
+
+}
+
+func(l *fifo)evict(c *cache){
+	fmt.Println("Evicting by fifo strtegy")
+}
+
+// 缓存策略的实现：fifo
+type lru struct {
+
+}
+
+func(l *lru)evict(c *cache){
+	fmt.Println("Evicting by lru strtegy")
+}
+
+// 缓存策略的实现：lfu
+type lfu struct {
+
+}
+
+func(l *lfu)evict(c *cache){
+	fmt.Println("Evicting by lfu strtegy")
+}
+
+// 在 cache 结构体中选择不同的缓存实现
+type cache struct {
+	storage map[string]string
+	evictionAlgo evictionAlgo  // 组合策略接口
+	capacity int
+	maxCapacity int
+}
+
+
+func(c *cache) setEvictionAlgo(e evictionAlgo){ // 设置一个缓存清除策略到cache中
+	c.evictionAlgo = e
+}
+
+func (c *cache)evict(){
+	c.evictionAlgo.evict(c)
+	c.capacity--
+}
+
+
+func(c *cache) add(key, value string){
+	if c.capacity == c.maxCapacity{ // 如果满了就执行清除策略
+		c.evict()
+	}
+	c.capacity++
+	c.storage[key]=value
+}
+
+
+func (c *cache) get(key string){
+	delete(c.storage, key)
+}
+
+
+func initCache() *cache{
+	storage := make(map[string]string)
+	return &cache{
+		storage:storage,
+		capacity:0,
+		maxCapacity:2,
+	}
+}
+
+
+func strategyExecutor(evict string){
+	cache := initCache()
+	switch evict {
+
+	case "lfu":
+		lfu := &lfu{}
+		cache.setEvictionAlgo(lfu)
+
+	case "lru":
+		lru := &lru{}
+		cache.setEvictionAlgo(lru)
+
+	case "fifo":
+		fifo := &fifo{}
+		cache.setEvictionAlgo(fifo)
+	}
+
+	cache.add("a", "1")
+	cache.add("b", "2")
+	cache.add("c", "3")
+	cache.add("d", "4")
+}
+
+
+func main() {
+
+	strategyExecutor("lru")
+	//strategyExecutor("fifo")
+	//strategyExecutor("lru")
+
+}
+
+
+
